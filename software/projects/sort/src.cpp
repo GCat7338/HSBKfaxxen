d0e333ce630a9c82eadd5f7d7a545688f929c1d5
GCat7338 <gamingcatsphone@gmail.com>
2025-10-06 17:32:18 +0200

    bogo bogo

--- software/projects/sort/src.cpp
+++ software/projects/sort/src.cpp
@@ -4,19 +4,19 @@
 #include "./../../global_libaries/sort/sort.h"
 
 int main() {
-	int array[10];
+	int array[5];
 	srand(time(0));
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 5; i++) {
 		array[i] = rand() % 100;
 	}
 
-	util::printArray(array, 10);
+	util::printArray(array, 5);
 
-	//sort::bubbleSort(array, 10);
-	std::cout << sort::bogoSort(array, 10) << std::endl;
+	//sort::bubbleSort(array, 5);
+	std::cout << sort::bogoSort(array, 5) << std::endl;
 
-	util::printArray(array, 10);
+	util::printArray(array, 5);
 
 	return 0;
 }
